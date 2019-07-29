8e5fc7bd3880feccdcf245131c96cda1fd0acbae
Ryan George <ryangeorge@Ryans-MacBook-Pro.local>
2019-07-28 23:31:52 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and running!");
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+    {
+    rank : "queen", 
+    suit : "hearts",
+    cardImage : "images/queen-of-hearts.png",
+    },
+    {
+    rank : "queen", 
+    suit : "diamonds",
+    cardImage : "images/queen-of-diamonds.png",
+    },
+    {
+    rank : "king", 
+    suit : "hearts",
+    cardImage: "images/king-of-hearts.png",
+    },
+    {
+    rank : "king", 
+    suit : "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+        }
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -16,8 +38,10 @@ checkForMatch();
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
 } else alert("Sorry, try again.")
-console.log("User flipped " + cards[cardId])
-cardsInPlay.push(cards[cardsId]);
+console.log("User flipped " + cards[cardId].rank)
+cardsInPlay.push(cards[cardsId].rank);
+console.log(cardImage);
+console.log(suit);
 };
 flipCard(0);
 flipCard(2);
