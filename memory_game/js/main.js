1844253b39512d7182c985cc7b1be302c56799d6
Ryan George <ryangeorge@Ryans-MacBook-Pro.local>
2019-07-28 19:51:53 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,6 +3,36 @@ console.log("Up and running!");
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
+};
+
+function flipCard(cardId) {
+checkForMatch();
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+} else alert("Sorry, try again.")
+console.log("User flipped " + cards[cardId])
+cardsInPlay.push(cards[cardsId]);
+};
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+/*
+
+console.log("Up and running!");
+
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+
 let cardOne = cards[0];
 cardsInPlay.push(cardOne);
 
@@ -11,13 +41,13 @@ console.log("User flipped " + cardOne);
 let cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-} else alert("Sorry, try again.")
-  /*  // Code to be executed if condition1 is false and condition2 is true
-   } else if (condition3) {
-    // Code to be executed if condition1 and condition2 are false and condition3 is true
-   } else {
-   // Code to be executed if condition1, condition2, and condition3 are false
-   } */
+
+
+};
+
+
+function checkForMatch() {
+
+};
+
+*/
